2a56e0a6818c5355b9333032e373a0361724a45b
IanZheng <flks_2005@qq.com>
2017-08-08 23:51:07 +0800

    add domain

---
+++ src/main/java/com/ian/sblog/domain/User.java
@@ -0,0 +1,13 @@
+package com.ian.sblog.domain;
+
+public class User {
+
+	private Integer id;
+	private String username;
+	private String password;
+	private String displayName;
+	private String realName;
+	private String email;
+	
+	
+}
